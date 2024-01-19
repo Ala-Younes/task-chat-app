fe474fc823ce06c17b8af8916847413b55c3cb57
Ala-Younes <ala.younes.ya@gmail.com>
2024-01-19 14:32:02 +0100

    0008 / FEATURE / routing pages logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,6 +21,7 @@ const Header = () => {
 
   const handleGoToPage = (page: string) => {
     goTo("/dashboard/" + page);
+    setCurrentPage(page);
   };
 
   // ! Get an element from the store useSelector
@@ -31,9 +32,14 @@ const Header = () => {
     goTo("/auth");
   }
 
-  function testn(): React.MouseEventHandler<HTMLButtonElement> | undefined {
-    throw new Error("Function not implemented.");
-  }
+  const setCurrentPage = (page: string) => {
+    localStorage.setItem("chat_app-page", page);
+  };
+
+  // ! Display elements depending on which page we are ...
+  const getCurrentPage = () => {
+    return localStorage.getItem("chat_app-page");
+  };
 
   return (
     <div className="flex flex-wrap z-10 sm:flex-row gap-5 items-center justify-between drop-shadow-md bg-gradient-to-r from-customBlue to-customPink px-5 py-5 md:py-2 text-white">
@@ -42,10 +48,39 @@ const Header = () => {
         src={logo}
         alt="logo"
       />
-      <div className="flex flex-wrap flex-row-reverse md:flex-row items-center justify-center gap-5">
-        <AddListBoard />
-        <Icon IconName={BsFillChatFill} reduceOpacityOnHover={false} ping />
-        <Icon IconName={FiList} reduceOpacityOnHover={false} />
+      <div className="flex flex-row-reverse md:flex-row items-center justify-center gap-5 flex-wrap">
+        {getCurrentPage() === "chat" ? (
+          <Icon
+            IconName={FiList}
+            onClick={() => handleGoToPage("")}
+            reduceOpacityOnHover={false}
+          />
+        ) : getCurrentPage() === "profile" ? (
+          <>
+            <Icon
+              reduceOpacityOnHover={false}
+              IconName={FiList}
+              onClick={() => handleGoToPage("")}
+            />
+            <Icon
+              IconName={BsFillChatFill}
+              ping
+              onClick={() => handleGoToPage("chat")}
+              reduceOpacityOnHover={false}
+            />
+          </>
+        ) : (
+          <>
+            <AddListBoard />
+            <Icon
+              IconName={BsFillChatFill}
+              ping
+              onClick={() => handleGoToPage("chat")}
+              reduceOpacityOnHover={false}
+            />
+          </>
+        )}
+
         <div className="group relative">
           <UserHeaderProfile user={currentUser} />
           <div className="absolute pt-5 hidden group-hover:block w-full min-w-max">
@@ -57,10 +92,9 @@ const Header = () => {
                 Profile
               </p>
               <button
-                className={`hover:bg-gray-200 w-full py-2 px-4  flex items-center gap-4 ${
-                  logoutLoading ? "cursor-wait" : "cursor-pointer"
-                }`}
-                onClick={!logoutLoading ? handleSignOut : undefined}
+                onClick={() => !logoutLoading && handleSignOut()}
+                // onClick={!logoutLoading ? handleSignOut : undefined}
+                className={`hover:bg-gray-200 w-full py-2 px-4 cursor-pointer flex items-center gap-4`}
               >
                 Logout
                 {logoutLoading && <Spinner />}
